d51ef570b965c74b0a0fb15d998c1613f80979bd
Johnny1992 <evgeny.frontend@gmail.com>
2019-09-03 19:13:45 +0300

    add: filtering

--- src/components/common/ListGroup/ListGroup.jsx
+++ src/components/common/ListGroup/ListGroup.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import './ListGroup.scss';
 
-const ListGroup = ({items, valueProperty, textProperty}) => {
+const ListGroup = ({items, valueProperty, textProperty, selectedGenre, onGenreSelect}) => {
     return (
         <ul className="list-group">
             {items.map(item => (
-                <li key={item[valueProperty]} className="list-group-item">
+                <li
+                    key={item[valueProperty]}
+                    className={item === selectedGenre ? "list-group-item active" : "list-group-item" }
+                    onClick={() => onGenreSelect(item)}
+                >
                     {item[textProperty]}
                 </li>
             ))}
