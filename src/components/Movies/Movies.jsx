47aa0a13f408dcff82722db265df9307ff4aec03
Johnny1992 <evgeny.frontend@gmail.com>
2019-09-04 10:05:05 +0300

    ref: table in movies => MoviesTable component

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import Like from './../common/Like/index';
 import ListGroup from '../common/ListGroup';
+import MoviesTable from './../common/MoviesTable';
 import Pagination from '../common/Pagination';
 import { getMovies } from '../../services/fakeMovieService';
 import { getGenres } from '../../services/fakeGenreService';
@@ -31,7 +31,7 @@ class Movies extends Component {
         });
     }
 
-    handleClickLike = (movie) => {
+    handleLike = (movie) => {
         const movies = [...this.state.movies];
         const index = movies.indexOf(movie);
         movies[index] = {...movies[index]};
@@ -78,44 +78,11 @@ class Movies extends Component {
                 </div>
                 <div className="col">
                     <p>Showing {filtered.length} movies in the database.</p>
-                    <table className='table'>
-                        <thead>
-                            <tr>
-                                <th>Title</th>
-                                <th>Genre</th>
-                                <th>Stock</th>
-                                <th>Rate</th>
-                                <th></th>
-                                <th></th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {
-                                movies.map(movie => (
-                                    <tr key={movie._id}>
-                                        <td>{movie.title}</td>
-                                        <td>{movie.genre.name}</td>
-                                        <td>{movie.numberInStock}</td>
-                                        <td>{movie.dailyRentalRate}</td>
-                                        <td>
-                                            <Like
-                                                liked={movie.liked}
-                                                onLikeToggle={() => this.handleClickLike(movie)}
-                                            />
-                                        </td>
-                                        <td><button
-                                            className="btn btn-danger btn-sm"
-                                            onClick={() => this.handleDelete(movie)}
-                                            >
-                                                Delete
-                                            </button>
-                                        </td>
-                                    </tr>
-                                ))
-                            }
-                            
-                        </tbody>
-                    </table>
+                    <MoviesTable
+                        movies={movies}
+                        onLike={this.handleLike}
+                        onDelete={this.handleDelete}
+                    />
                     <Pagination
                         itemsCount={filtered.length}
                         pageSize={pageSize}
