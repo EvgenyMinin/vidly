08e86cccd84b10dd9af834811222d37e51ff0790
Johnny1992 <evgeny.frontend@gmail.com>
2019-09-10 11:10:10 +0300

    add: controlled element in LoginForm

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -2,14 +2,42 @@ import React, { Component } from 'react';
 import './LoginForm.scss';
 
 class LoginForm extends Component {
-    render() { 
+
+    state = {
+        account: {
+            username: '',
+            password: ''
+        }
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+    }
+
+    handleChange = (e) => {
+        const account = {...this.state.account};
+        account.username = e.currentTarget.value;
+        this.setState({
+            account
+        });
+    }
+
+    render() {
+        const { username } = this.state.account;
         return (
             <div>
                 <h1>Login</h1>
-                <form>
+                <form onSubmit={this.handleSubmit}>
                     <div className="form-group">
                         <label htmlFor="username">Username</label>
-                        <input id="username" type="text" className="form-control"/>
+                        <input
+                            autoFocus
+                            className="form-control"
+                            id="username"
+                            type="text"
+                            value={username}
+                            onChange={this.handleChange}
+                        />
                     </div>
                     <div className="form-group">
                         <label htmlFor="password">Password</label>
