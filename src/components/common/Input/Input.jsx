38ebb0687df24a126241edb0850814b504d53363
Johnny1992 <evgeny.frontend@gmail.com>
2019-09-11 11:57:54 +0300

    add: validating LoginForm using Joi

--- src/components/common/Input/Input.jsx
+++ src/components/common/Input/Input.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import './Input.scss';
 
-const Input = ({ name, label, value, onChange }) => {
+const Input = ({ name, label, value, error, onChange }) => {
     return (
         <div className="form-group">
             <label htmlFor={name}>{label}</label>
             <input
                 className="form-control"
                 id={name}
+                name={name}
                 type='text'
                 value={value}
                 onChange={onChange}
             />
+            {error && <div className="alert alert-danger">{error}</div>}
         </div>
     );
 }
